dd86fb0ade9015b584aae64870dc9e32e154a120
Hadasa Matan <hadasamatan@gmail.com>
2025-05-29 23:21:03 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/4U/',
-  // שאר ההגדרות שלך
+  plugins: [react()],
+  base: '/4U/'
 });
