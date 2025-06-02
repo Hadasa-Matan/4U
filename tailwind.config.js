814004bd26b10738704ca4b39ad99d2fe10011ba
Hadasa Matan <hadasamatan@gmail.com>
2025-06-03 00:13:16 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        custom: ['FbAsparagos', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
